040916f3b62ab5fd7f6d1cf435e1c0199d8b60e9
Callum Alton <callum.alton33@gmail.com>
2020-05-28 17:07:55 +0100

    added css and hbs

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express');
+const hbs = require('express-handlebars')
 const app = express();
+const path = require('path')
 
 require('dotenv').config();
 
-const getWeather = require('./openWeatherMap');
+const getWeather = require('./lib/openWeatherMap');
+
+const getNasaData = require('./lib/nasa')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.engine('.hbs', hbs({
+    defaultLayout: 'layout',
+    extname: 'hbs'
+}))
+
+app.set('view engine', 'hbs')
 
 app.get('/', async(req, res) => {
-    
-    let data = await getWeather();
-    res.send(data)
+    let data = await getWeather.getWeather()
+    console.log(data)
+    let name = data.name
+    let temp = Math.ceil(data.main.temp - 273.15)
+    let description = data.weather[0].description
+    let clouds = data.clouds.all
+    res.render('index', {name, description, temp, clouds})
 });
+
+app.get('/nasa', async(req, res) => {
+    let data = await getNasaData.getNasaData()
+    let explanation = data.explanation
+    console.log(data)
+    res.render('nasa', {explanation})
+})
 
 app.get('/page2', (req, res) => {
     res.send('I am the second page')
